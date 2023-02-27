7f626c5631c1f855f67242fe6da44d9a95e661cf
Lilia <liliagarciahiramatsu@gmail.com>
2023-02-26 22:18:48 -0300

    detalles en home y contact

--- index.js
+++ index.js
@@ -1,21 +1,36 @@
-const labelCategoria = document.getElementById('category')
-
 const contenedorTarjetas = document.getElementById('contenedor')
 
 let tarjetas = ''
+let fechaActual = eventos.currentDate
 
 for (const evento of eventos.events) {
-    tarjetas += `<div class="card carta" style="width: 18rem;">
-                    <img src="${evento.image}" class="card-img-top" alt="">
-                    <div class="card-body">
-                        <h5 class="card-title">${evento.name}</h5>
-                        <p class="card-text">${evento.description}</p>
-                    </div>
-                    <div class="card-body">
-                        <span class="price">Price: $${evento.price}</span>
-                        <a class="card-link btn btn-outline-primary" href="./details.html" role="button">See more</a>
-                    </div>
-                </div>`
+    if (fechaActual > evento.date) {
+        tarjetas += `<div class="card carta" style="width: 18rem;">
+                        <img src="${evento.image}" class="card-img-top" alt="">
+                        <div class="card-body">
+                            <h5 class="card-title">${evento.name}</h5>
+                            <p class="card-text">${evento.description}</p>
+                        </div>
+                        <div class="card-body">
+                            <span class="price" style="color:red">Ended</span>
+                            <a class="card-link btn btn-outline-primary" href="./details.html" role="button">See more</a>
+                        </div>
+                    </div>`
+    }
+    else {
+        tarjetas += `<div class="card carta" style="width: 18rem;">
+                        <img src="${evento.image}" class="card-img-top" alt="">
+                        <div class="card-body">
+                            <h5 class="card-title">${evento.name}</h5>
+                            <p class="card-text">${evento.description}</p>
+                        </div>
+                        <div class="card-body">
+                            <span class="price">Price: $${evento.price}</span>
+                            <a class="card-link btn btn-outline-primary" href="./details.html" role="button">See more</a>
+                        </div>
+                    </div>`
+    }
+    
 }
 
 contenedorTarjetas.innerHTML = tarjetas
